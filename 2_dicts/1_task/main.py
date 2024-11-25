5e149f8682acf7ed375149f37ca0a51d40e53f0a
Polina Volkova <158420945+PolinaVolkova100@users.noreply.github.com>
2024-11-25 15:26:59 +0300

    Update main.py

--- 2_dicts/1_task/main.py
+++ 2_dicts/1_task/main.py
@@ -19,8 +19,25 @@ def get_employees_info() -> list[str]:
 
 def get_parsed_employees_info() -> list[dict[str, int | str]]:
     """Функция парсит данные, полученные из внешнего API и приводит их к стандартизированному виду."""
-    _ = get_employees_info()
+    info = get_employees_info()
+    info = [sentence.split() for sentence in info]
     parsed_employees_info = []
-
-    # Ваш код ниже
+    for lst in info:
+        dct = {
+            "id": 0,
+            "name": "",
+            "last_name": "",
+            "age": 0,
+            "salary": 0,
+            "position": "",
+        }
+        for i in range(0, len(lst), 2):
+            if lst[i] in dct:
+                if lst[i] == "id" or lst[i] == "age":
+                    dct[lst[i]] = int(lst[i + 1])
+                elif lst[i] == "salary":
+                    dct[lst[i]] = float(lst[i + 1])
+                else:
+                    dct[lst[i]] = lst[i + 1]
+        parsed_employees_info.append(dct)
     return parsed_employees_info
