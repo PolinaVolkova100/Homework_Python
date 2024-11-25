bdd66013da0ffdd08e6ebc8673560d05d29840ef
Polina Volkova <158420945+PolinaVolkova100@users.noreply.github.com>
2024-11-25 15:32:23 +0300

    Update encode.py

--- 3_functions/1_phone_text_code/encode/encode.py
+++ 3_functions/1_phone_text_code/encode/encode.py
@@ -1,4 +1,23 @@
 def encode_text(text: str) -> str | None:
-    """Пишите ваш код здесь."""
+    symbols = {
+        " ": "0",
+        ".,?!:;": "1",
+        "абвг": "2",
+        "дежз": "3",
+        "ийкл": "4",
+        "мноп": "5",
+        "рсту": "6",
+        "фхцч": "7",
+        "шщъы": "8",
+        "ьэюя": "9",
+    }
     result = ""
+    symbols_keys = list(list(symbols_key) for symbols_key in symbols.keys())
+    for s in text:
+        if s not in "".join(list(symbols.keys())):
+            result = "None"
+            break
+        for i in range(len(symbols_keys)):
+            if s in symbols_keys[i]:
+                result += str(i) * (symbols_keys[i].index(s) + 1) + " "
     return result
