2fce88f3d150a3e68f796a91f35dec2df28a2335
Polina Volkova <158420945+PolinaVolkova100@users.noreply.github.com>
2024-11-25 15:28:48 +0300

    Update main.py

--- 2_dicts/2_task/main.py
+++ 2_dicts/2_task/main.py
@@ -7,5 +7,17 @@ def top_10_most_common_words(text: str) -> dict[str, int]:
     Returns:
         словарь типа {слово: количество вхождений}
     """
+    text = [word.strip('!?,.$%#^-;":') for word in text.lower().split()]
     most_common = {}
+
+    for word in text:
+        if len(word) > 2:
+            most_common[word] = most_common.get(word, 0) + 1
+
+    most_common = dict(sorted(most_common.items(), key=lambda value: value[0]))
+    most_common = dict(
+        sorted(most_common.items(), key=lambda value: value[1], reverse=True)
+    )
+    most_common = dict(list(most_common.items())[:10])
+
     return most_common
