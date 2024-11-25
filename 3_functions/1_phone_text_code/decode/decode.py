5ea0bb248040910433d8298c4dd87390027a6dc9
Polina Volkova <158420945+PolinaVolkova100@users.noreply.github.com>
2024-11-25 15:31:28 +0300

    Update decode.py

--- 3_functions/1_phone_text_code/decode/decode.py
+++ 3_functions/1_phone_text_code/decode/decode.py
@@ -1,4 +1,25 @@
 def decode_numbers(numbers: str) -> str | None:
     """Пишите ваш код здесь."""
+    symbols = {
+        "1": ".,?!:;",
+        "2": "абвг",
+        "3": "дежз",
+        "4": "ийкл",
+        "5": "мноп",
+        "6": "рсту",
+        "7": "фхцч",
+        "8": "шщъы",
+        "9": "ьэюя",
+        "0": " ",
+    }
+    numbers = numbers.split()
     result = ""
+    for number in numbers:
+        if number in [symbol * len(number) for symbol in symbols.keys()] and len(
+            number
+        ) <= len(symbols[number[0]]):
+            result += symbols[number[0]][len(number) - 1]
+        else:
+            result = "None"
+            break
     return result
