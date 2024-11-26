75308529d25ef9dd0da8f28f18d2be70e396fe86
Polina Volkova <158420945+PolinaVolkova100@users.noreply.github.com>
2024-11-26 17:36:43 +0300

    Update encode.py

--- 3_functions/1_phone_text_code/encode/encode.py
+++ 3_functions/1_phone_text_code/encode/encode.py
@@ -11,13 +11,14 @@ def encode_text(text: str) -> str | None:
         "шщъы": "8",
         "ьэюя": "9",
     }
-    result = ""
+    result = []
     symbols_keys = list(list(symbols_key) for symbols_key in symbols.keys())
     for s in text:
         if s not in "".join(list(symbols.keys())):
-            result = "None"
+            return None
             break
         for i in range(len(symbols_keys)):
             if s in symbols_keys[i]:
-                result += str(i) * (symbols_keys[i].index(s) + 1) + " "
+                result.append(str(i) * (symbols_keys[i].index(s) + 1))
+    result = " ".join(result)
     return result
