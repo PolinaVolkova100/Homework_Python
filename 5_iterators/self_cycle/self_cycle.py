949b1d665f57e92e50327d135a2e5ccf672241d4
Polina Volkova <158420945+PolinaVolkova100@users.noreply.github.com>
2024-12-21 21:13:43 +0300

    Update self_cycle.py

--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -2,11 +2,23 @@ from typing import Generator, Iterable, TypeVar
 
 T = TypeVar("T")
 
-
 def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
-    """Пишите ваш код здесь."""
-
+    while obj:
+        for el in obj:
+            yield el
 
 class Cycle:
     def __init__(self, obj: Iterable[T]):
-        """Реализуйте класс"""
+        self.obj = tuple(obj)
+        self.len = len(obj)
+        self.index = 0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index < self.len:
+            self.index += 1
+            return self.obj[self.index - 1]
+        elif self.index == self.len:
+            self.index = 1
+            return self.obj[0]
+
